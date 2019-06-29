e066f5d745a5366393403272570a1e646baeebf6
Marco Frisina <marco@frisina.xyz>
2019-06-29 23:58:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,6 @@ def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  
+  return array.include?(cheese_types)
 
 end
