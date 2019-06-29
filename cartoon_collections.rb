f365988bd3ec8b42dcb7bfeef10cea5b04960a64
Marco Frisina <marco@frisina.xyz>
2019-06-29 23:53:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,9 @@ def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
+  superset = (array + cheese_types)
+  subset = superset.uniq
 
-
+  return subset
 
 end
