df7954f82afbdaf5308e5dbbdb3751c312c7dc1b
Marco Frisina <marco@frisina.xyz>
2019-06-29 22:37:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,12 +18,9 @@ end
 
 def long_planeteer_calls(array) # code an argument here
   # Your code here
-  return_value = false
 
-  if array.select { |num| num.length > 4 }
-    return_value = true
-  end
-  return return_value
+  return array.any? { |num| num > 4}
+
 end
 
 
