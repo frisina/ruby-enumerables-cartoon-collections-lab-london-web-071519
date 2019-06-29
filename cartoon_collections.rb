174af60d051152c6f041e723b632ea99f031d686
Marco Frisina <marco@frisina.xyz>
2019-06-29 22:49:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,6 @@ def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  return array.replace(cheese_types)
+  return cheese_types.replace(array)
 
 end
