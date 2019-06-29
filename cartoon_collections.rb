475592f6b4a6824d9fd13a45191c85feac7352fe
Marco Frisina <marco@frisina.xyz>
2019-06-29 22:48:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,10 @@ def long_planeteer_calls(array) # code an argument here
 end
 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  return array.include?(cheese_types)
+
 end
