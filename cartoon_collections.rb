aa4f1739d1fe5a7464c767112c3fb1bc61fbc4d2
Marco Frisina <marco@frisina.xyz>
2019-06-30 00:22:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ def find_the_cheese(array) # code an argument here
   result = nil
   new_list = []
 
-  if array.include?(cheese_types)
+  if array.compare(cheese_types)
     new_list = array & cheese_types
     result = new_list[0]
   else
